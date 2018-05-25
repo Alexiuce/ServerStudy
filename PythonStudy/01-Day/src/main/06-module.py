da968a79042e20c66daa5af08725104d498f5613
Alexiuce <alexiuce@qq.com>
2018-05-25 10:02:49 +0800

    update module haslib md5

--- PythonStudy/01-Day/src/main/06-module.py
+++ PythonStudy/01-Day/src/main/06-module.py
@@ -1,7 +1,14 @@
 
 import sys  # 导入系统模块
 import msgkit
+import hashlib
+
 
 print(sys.argv)   #获取应用传递进来的参数 列表类型,第一个参数是应用文件本身
 print("06 ")
 msgkit.HelloModel.say()
+
+help(hashlib.md5)
+p = hashlib.md5("joinf".encode("utf8"))
+print(p.hexdigest())
+
