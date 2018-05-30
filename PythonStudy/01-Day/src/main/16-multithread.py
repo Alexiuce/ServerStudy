71fb756bd1af5d29d293c658202fa10ccdab66fc
Alexiuce <alexiuce@qq.com>
2018-05-30 09:33:54 +0800

    sync task

--- PythonStudy/01-Day/src/main/16-multithread.py
+++ PythonStudy/01-Day/src/main/16-multithread.py
@@ -12,7 +12,7 @@ release() : 解锁
 
 """
 
-from threading import Thread
+from threading import Thread, Lock
 import time
 
 def say():
@@ -27,6 +27,50 @@ class MThread(Thread):
             time.sleep(1)
 
 
+"""线程同步"""
+lock1 = Lock()     # 创建互斥锁1
+lock2 = Lock()     # 创建互斥锁2
+lock2.acquire()    # 设置加锁状态
+lock3 = Lock()     # 创建互斥锁3
+lock3.acquire()     # 设置加锁状态
+
+
+
+class Task_one(Thread):
+
+    def run(self):
+        while True:
+            if lock1.acquire():
+                print("doing...task one.....")
+                time.sleep(0.5)
+                lock2.release()   # 解锁lock2
+
+class Task_two(Thread):
+    def run(self):
+        while True:
+            if lock2.acquire():
+                print("doing task two.....")
+                time.sleep(0.5)
+                lock3.release()   # 解锁lock3
+
+class Task_three(Thread):
+    def run(self):
+        while True:
+            if lock3.acquire():
+                print("doing task three....")
+                time.sleep(0.5)
+                lock1.release()
+
+
+def sync_test():
+    t1 = Task_one()
+    t2 = Task_two()
+    t3 = Task_three()
+
+    t1.start()
+    t2.start()
+    t3.start()
+
 def test():
     for _ in range(5):
         th = MThread()
@@ -39,3 +83,4 @@ if __name__ == '__main__':
     test()
     help(Thread)
     print("main")
+    sync_test()
