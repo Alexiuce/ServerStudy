70b1907a9ca081adb2f136b875387c2093e30aac
Alexiuce <alexiuce@qq.com>
2018-05-31 14:23:18 +0800

    udp sendto and recefrom

--- PythonStudy/01-Day/src/main/17-tcpip.py
+++ PythonStudy/01-Day/src/main/17-tcpip.py
@@ -13,8 +13,15 @@ def upd_socket_test():
     s = socket(AF_INET,SOCK_DGRAM)   # 创建socket 对象, 使用udp协议
     host = ("10.211.55.3",8080)    # 设置目标主机与端口
     s.sendto(b"hello",host)     # 发送数据到目标主机
+    s.sendto(b'world',host)
     s.close()   # 关闭socket
 
+def upd_receive():
+    sc = socket(AF_INET, SOCK_DGRAM)
+    port = ("", 9988)
+    sc.bind(port)
+    rece_data = sc.recvfrom(1024)
+    print(rece_data)
 
 if __name__ == '__main__':
     upd_socket_test()
