41de526549836fd270243f1dcd8c1db71fbe3ff7
Alexiuce <alexiuce@qq.com>
2018-05-31 14:50:18 +0800

    string endcode

--- PythonStudy/01-Day/src/main/17-tcpip.py
+++ PythonStudy/01-Day/src/main/17-tcpip.py
@@ -12,6 +12,13 @@ from socket import *
 def upd_socket_test():
     s = socket(AF_INET,SOCK_DGRAM)   # 创建socket 对象, 使用udp协议
     host = ("10.211.55.3",8080)    # 设置目标主机与端口
+    """
+    sendto(bytes,port): 
+    当发送字符串时,需要使用encode('utf-8') 对字符串进行编码后才可以发送
+    可以在字符串前使用b来作为编码标识
+    
+    接收到字符串时,如果需要解码,需要使用decode("编码格式") 进行解码
+    """
     s.sendto(b"hello",host)     # 发送数据到目标主机
     s.sendto(b'world',host)
     s.close()   # 关闭socket
