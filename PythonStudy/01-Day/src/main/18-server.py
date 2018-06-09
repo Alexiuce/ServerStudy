c3bc296923c013ac0708dff9b004328fed6ce36f
Alexiuce <alexiuce@qq.com>
2018-06-09 19:25:03 +0800

    update

--- PythonStudy/01-Day/src/main/18-server.py
+++ PythonStudy/01-Day/src/main/18-server.py
@@ -1,5 +1,16 @@
 # 18-server.py
 # Created by Alexcai at 2018/6/9
-"""  服务器
+"""  服务器 单进程 非阻塞
 
 """
+
+
+from socket import *
+
+
+if __name__ == '__main__':
+    sc = socket(AF_INET,SOCK_STREAM)
+    port = ('',23456)
+    sc.bind(port)
+    sc.setblocking(False)  # 设置套接字非阻塞
+    sc.listen(5)
