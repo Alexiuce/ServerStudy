f9390d43fe7257436863d9b38923683c24395a14
Alexiuce <alexiuce@qq.com>
2018-06-16 17:56:37 +0800

    update

--- PythonStudy/01-Day/src/main/21-dynamicHttpServer.py
+++ PythonStudy/01-Day/src/main/21-dynamicHttpServer.py
@@ -5,17 +5,46 @@
 dynamic web server
 
 """
+from socket import *
+from multiprocessing import Process
+
+
+res = """HTTP/1.1 200 OK
+
+<html>
+Hello Python ...
+</html>
+"""
 
 class WebServer(object):
-    pass
+
+    def __init__(self,port):
+        self.__port__ = port
+        server_sock = socket(AF_INET,SOCK_STREAM)
+        server_sock.bind(('',port))
+        self.__server_sock__ = server_sock
+
+    def start(self):
+        self.__server_sock__.listen(5)
+        while True:
+            client_sock,client_addr = self.__server_sock__.accept()
+            p = Process(target=self.handle_resquest,args=(client_sock,))
+            p.start()
+            client_sock.close()
 
 
+    def handle_resquest(self,client):
+        request_data = client.recv(1024)
+        print(request_data)
+        client.send(res.encode('utf8'))
+        client.close()
 
 
 
 
 def main():
-    pass
+    web = WebServer(6789)
+    web.start()
 
 if __name__ == '__main__':
     main()
