e0cf2fd8efc6c4c8f527737610d06dbd38831125
Alexiuce <alexiuce@qq.com>
2018-06-20 10:45:21 +0800

    get attr for class

--- PythonStudy/01-Day/src/main/22-webframework.py
+++ PythonStudy/01-Day/src/main/22-webframework.py
@@ -12,11 +12,28 @@ class Application(object):
         self.__routers__ = routers
 
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, env, start_response):
+        path = env.get('PATH_INFO','/')
+        # 路由逻辑判断
+          # 1. 判断静态文件
+
+        # 遍历路由,查找匹配的执行方法
+        for url, handle in self.__routers__:
+            if url == path:
+                return handle(env,start_response)
+        # 错误处理
+        status = "404 Not Found"
+        headers = []
+        start_response(status,headers)
+        return "Path not exist"
 
 
+""" getattr 
 
+获取对象的属性内容:
+getattr(objc, attrname) : 获取对象objc的attrname属性
+
+"""
 
 
 
