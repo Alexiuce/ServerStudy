f1e397d2bc0e852a1218f5116fcee9db64760cc8
Alexiuce <alexiuce@qq.com>
2018-06-20 10:05:00 +0800

    update web framework

--- PythonStudy/01-Day/src/main/22-webframework.py
+++ PythonStudy/01-Day/src/main/22-webframework.py
@@ -4,3 +4,36 @@
 """Web 框架"""
 
 
+
+
+class Application(object):
+
+    def __init__(self,routers):
+        self.__routers__ = routers
+
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+
+
+
+
+def application(env,start_response):
+    urls = [
+        ('/hello',handle_hello)
+    ]
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
