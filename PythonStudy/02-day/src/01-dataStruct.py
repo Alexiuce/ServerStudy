788dd919a09bfb7a1997f8f2f29a0f224120a539
Alexiuce <alexiuce@qq.com>
2018-06-24 11:22:22 +0800

    kotlin merge

---
+++ PythonStudy/02-day/src/01-dataStruct.py
@@ -0,0 +1,37 @@
+# 01-dataStruct.py
+# Created by Alexcai at 2018/6/24
+
+""" 练习题1
+如果a + b + c = 1000 ,且a^2 + b^2 = c^2 (a,b,c 为自然数),如何求出所有a,b,c的组合
+
+"""
+
+def sample_1():
+
+    # r1 = (x for x in range(1001))
+    # r2 = (x for x in range(1001))
+    # r3 = (x for x in range(1001))
+    rang1 = range(1001)
+    
+    # for a in rang1:
+    #     for b in rang1:
+    #         for c in rang1:
+    #             if a + b + c == 1000 and a**2 + b**2 == c**2:
+    #                 print("a=%d b=%d c=%d" %(a,b,c))
+
+
+    for a in rang1:
+        for b in rang1:
+            c = 1000 - q - b
+            if  a**2 + b**2 == c**2:
+                print("a=%d b=%d c=%d" %(a,b,c))
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    sample_1()
