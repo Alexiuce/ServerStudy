b0c7be52071e0d4f99c0ef1914301cd6b688b96c
秀策 <caijinzhu@dong10.com>
2019-05-11 10:51:46 +0800

    feat(#Python): regex 01

--- PythonStudy/01-Day/01-hello.py
+++ PythonStudy/01-Day/01-hello.py
@@ -1,3 +1,10 @@
 #coding=utf8
+
+import re
+
+s = 'Hello world for your python'
 print("hello  python")
-print("世界 你好")
+# print("世界 你好")
+m = re.match('H',s)
+if m is not None:
+    print(m.group())
