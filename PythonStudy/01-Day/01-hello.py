cb1026565a6dd46b2066feab498dc7b3a97c6ed2
秀策 <caijinzhu@dong10.com>
2019-05-12 10:53:39 +0800

    feat(#Python): regex  | . 匹配

--- PythonStudy/01-Day/01-hello.py
+++ PythonStudy/01-Day/01-hello.py
@@ -2,9 +2,29 @@
 
 import re
 
-s = 'Hello world for your python'
+s = 'Hello worlds for your python'
 print("hello  python")
 # print("世界 你好")
-m = re.match('H',s)
+m = re.match('world',s)
 if m is not None:
     print(m.group())
+
+
+n = re.search('world',s)
+if n is not None:
+    print(n.group())
+#  分组 | 模式
+ps = "the dog sit down on the bat back~"
+pt = 'bat|bet|bit'
+pm = re.match(pt,'bit')
+if pm is not None: 
+    print(pm.group())
+sm = re.search(pt,ps)
+if sm is not None: 
+    print(sm.group())
+
+#  . 匹配除\n之外的任何字符(空白字符除外)
+pd = '.the'
+pdm = re.match(pd,ps)
+if pdm is not None:
+    print(pdm.group())
