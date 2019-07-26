f405d76fb9ecb1751437ddf63decff13b59a106c
caijinzhu <alexiuce@qq.com>
2019-07-26 16:51:10 +0700

    feat(#Spring-cloud demo): 添加父工程

---
+++ SpringBoot/02-SpringCloud/eureka-server/src/main/java/server/MEurekaServer.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MEurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(MEurekaServer.class,args);
+    }
+
+}
