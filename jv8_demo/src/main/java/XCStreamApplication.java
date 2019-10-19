dc39e9f3f8a54f5b87629036a6941d7444708908
caijinzhu <alexiuce@qq.com>
2019-10-19 09:23:01 +0700

    feat(#java stream): parallelSteam

--- jv8_demo/src/main/java/XCStreamApplication.java
+++ jv8_demo/src/main/java/XCStreamApplication.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class XCStreamApplication {
@@ -5,15 +6,33 @@ public class XCStreamApplication {
 
     public static void main(String[] args) {
 
-        Stream.of("d2", "a2", "b1", "b3", "c")
+//        Stream.of("d2", "a2", "b1", "b3", "c")
+//                .filter(s -> {
+//                    System.out.println("filter: " + s);
+//                    return true;
+//                })
+//                .forEach(s -> System.out.println("forEach: " + s));
+
+
+        Arrays.asList("a1", "a2", "b1", "c2", "c1")
+                .parallelStream()
                 .filter(s -> {
-                    System.out.println("filter: " + s);
+                    System.out.format("filter: %s [%s]\n",
+                            s, Thread.currentThread().getName());
                     return true;
                 })
-                .forEach(s -> System.out.println("forEach: " + s));
-
-
-
+                .map(s -> {
+                    System.out.format("map: %s [%s]\n",
+                            s, Thread.currentThread().getName());
+                    return s.toUpperCase();
+                })
+                .sorted((s1, s2) -> {
+                    System.out.format("sort: %s <> %s [%s]\n",
+                            s1, s2, Thread.currentThread().getName());
+                    return s1.compareTo(s2);
+                })
+                .forEach(s -> System.out.format("forEach: %s [%s]\n",
+                        s, Thread.currentThread().getName()));
     }
 
 
