a9b7168e782b1adfe33e1092281dcd21011bad51
caijinzhu <alexiuce@qq.com>
2019-11-08 08:02:14 +0700

    feat(#add change slider value for img):

--- RSA/RSADemo/CoreImageDemo/ViewController.swift
+++ RSA/RSADemo/CoreImageDemo/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var mcontext: CIContext!
+    var mfilter: CIFilter!
+    var beginImg: CIImage!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let image = UIImage(named: "image")!
@@ -21,6 +26,7 @@ class ViewController: UIViewController {
         cFilter.setValue(0.5, forKey: kCIInputIntensityKey)
         guard let outputImage = cFilter.outputImage else { return  }
         
+        
 //        let newImage = UIImage(ciImage: outputImage)
 //        self.imageView.image = newImage
         
@@ -28,13 +34,24 @@ class ViewController: UIViewController {
         guard let cgImg = context.createCGImage(outputImage, from: outputImage.extent) else {return}
         imageView.image = UIImage(cgImage: cgImg)
         
+        mcontext = context
+        mfilter = cFilter
+        beginImg = cImage
+        
+        
     }
     
     
     
     @IBAction func changeSlider(_ sender: UISlider) {
         print("value = \(sender.value)")
-        
+        let sliderValue = sender.value;
+        mfilter.setValue(sliderValue, forKey: kCIInputIntensityKey)
+        guard let outputImg = mfilter.outputImage else {return}
+        guard let cgImg = mcontext.createCGImage(outputImg, from: outputImg.extent) else {return}
+        let img = UIImage(cgImage: cgImg)
+        imageView.image = img
+    
     }
     
     
