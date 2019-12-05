8c08164849d64eac7b6ffe717f0f1e5b62ba9dab
caijinzhu <alexiuce@qq.com>
2019-12-05 09:49:48 +0700

    feat(#add collection layout):

---
+++ RSA/RSADemo/Compositional Layouts/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Compositional Layouts
+//
+//  Created by alexiuce on 2019/12/5.
+//  Copyright © 2019 alexiuce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
+extension ViewController{
+    
+    fileprivate func generateColletionLayout()-> UICollectionViewLayout{
+        /** item size */
+        let itemSize = NSCollectionLayoutSize(widthDimension: <#T##NSCollectionLayoutDimension#>, heightDimension: <#T##NSCollectionLayoutDimension#>)
+        
+        
+    }
+    
+    
+}
+
