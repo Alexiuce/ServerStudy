11b9a11a9b68b6df98e42cc4995811de63a09a55
caijinzhu <alexiuce@qq.com>
2020-01-11 18:24:37 +0800

    feat(#监听屏幕横竖变化):

--- XCTimeClock/XCTimeClock/ViewController.m
+++ XCTimeClock/XCTimeClock/ViewController.m
@@ -8,6 +8,14 @@
 
 #import "ViewController.h"
 
+
+
+#define ScreenWidth   UIScreen.mainScreen.bounds.size.width
+#define ScreenHeight  UIScreen.mainScreen.bounds.size.height
+
+
+
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *hourView;              /** 小时部分view */
 @property (weak, nonatomic) IBOutlet UIView *minuteView;            /** 分钟部分view */
@@ -19,9 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self updateTimeText];
-    
-    
-    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(onChangeDirection:) name:UIDeviceOrientationDidChangeNotification object:nil];
+//    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(onChangeDirection:) name:UIDeviceOrientationDidChangeNotification object:nil];
     
 }
 
@@ -29,23 +35,54 @@
     
 }
 
-#pragma mark - Notification handler
-- (void)onChangeDirection:(NSNotification *)noti{
+#pragma mark - view will transition
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator{
     UIDeviceOrientation orientation = UIDevice.currentDevice.orientation;
     switch (orientation) {
         case UIDeviceOrientationPortrait:
             NSLog(@"protraint");
+            
+            NSLog(@"%@",NSStringFromCGSize(size));
+            
             break;
         case UIDeviceOrientationLandscapeLeft:
             NSLog(@"landscapeLeft");
+            NSLog(@"%@",NSStringFromCGSize(size));
             break;
         case UIDeviceOrientationLandscapeRight:
             NSLog(@"landscapeRight");
+            NSLog(@"%@",NSStringFromCGSize(size));
             break;
             
         default:
             break;
     }
+    
+}
+
+
+#pragma mark - Notification handler
+- (void)onChangeDirection:(NSNotification *)noti{
+//    UIDeviceOrientation orientation = UIDevice.currentDevice.orientation;
+//    switch (orientation) {
+//        case UIDeviceOrientationPortrait:
+//            NSLog(@"protraint");
+//
+//            NSLog(@"width = %f, height = %f",ScreenWidth,ScreenHeight);
+//
+//            break;
+//        case UIDeviceOrientationLandscapeLeft:
+//            NSLog(@"landscapeLeft");
+//            NSLog(@"width = %f, height = %f",ScreenWidth,ScreenHeight);
+//            break;
+//        case UIDeviceOrientationLandscapeRight:
+//            NSLog(@"landscapeRight");
+//            NSLog(@"width = %f, height = %f",ScreenWidth,ScreenHeight);
+//            break;
+//
+//        default:
+//            break;
+//    }
 }
 
 
